436d5e8eab999d2fde4023e4e069e398326bc8a2
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2023-03-10 20:18:27 -0500

    Added HR service files

---
+++ HumanResourcesService/src/test/java/com/example/humanresourcesservice/HumanResourcesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.humanresourcesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
